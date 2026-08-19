ad3b3b4a0a16b5e482955e8f70e93d46503e44c3
thakuramitsingh9365-ctrl <thakuramitsingh9365@gmail.com>
2026-08-19 13:21:23 +0530

    Update Hello

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Varanasi Software Junction');
+  res.end('Hello Node');
 });
 
 server.listen(port, hostname, () => {
