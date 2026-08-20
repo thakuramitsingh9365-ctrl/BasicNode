438e24a1885b47d297cf85a3e307b8e709c0724c
thakuramitsingh9365-ctrl <thakuramitsingh9365@gmail.com>
2026-08-20 12:20:33 +0530

    GET and POST

--- server.js
+++ server.js
@@ -2,11 +2,24 @@ const express = require("express");
 const server = express();
 const hostname = "127.0.0.1";
 const port = 3000;
-server.get("/", (req, res) => {
-  const queryObject = req.query;
-  res.setHeader("Content-Type", "application/json");
 
-  res.send(JSON.stringify(queryObject));
+server.use(express.urlencoded({ extended: true }));
+
+server.get("/", (req, res) => {
+  res.send(`
+    <form method="POST" action="/submit">
+      <label for="name">Name:</label>
+      <input type="text" id="name" name="name"><br><br>
+      <label for="email">Email:</label>
+      <input type="email" id="email" name="email"><br><br>
+      <button type="submit">Submit</button>
+    </form>
+  `);
+});
+
+server.post("/submit", (req, res) => {
+  const { name, email } = req.body;
+  res.send(`Name: ${name}, Email: ${email}`);
 });
 
 server.listen(port, hostname, () => {
