b41e4c8286ff876ca75fb09027c8cb6acf04178c
thakuramitsingh9365-ctrl <thakuramitsingh9365@gmail.com>
2026-08-20 12:10:34 +0530

    Get using Express

--- server.js
+++ server.js
@@ -1,47 +1,13 @@
-const http = require('http');
-
-const hostname = '127.0.0.1';
+const express = require("express");
+const server = express();
+const hostname = "127.0.0.1";
 const port = 3000;
+server.get("/", (req, res) => {
+  const queryObject = req.query;
+  res.setHeader("Content-Type", "application/json");
 
-const server = http.createServer((request, response) => {
-
-
-  const { headers, method, url } = request;
-  let body = [];
-  request.on('error', (err) => {
-    console.error(err);
-  }).on('data', (chunk) => {
-    body.push(chunk);
-  }).on('end', () => {
-    body = Buffer.concat(body).toString();
-    // BEGINNING OF NEW STUFF
-
-    response.on('error', (err) => {
-      console.error(err);
-    });
-
-    response.statusCode = 200;
-    response.setHeader('Content-Type', 'application/json');
-    // Note: the 2 lines above could be replaced with this next one:
-    // response.writeHead(200, {'Content-Type': 'application/json'})
-
-    const responseBody = { headers, method, url, body };
-
-    response.write(JSON.stringify(responseBody));
-    response.end();
-    // Note: the 2 lines above could be replaced with this next one:
-    // response.end(JSON.stringify(responseBody))
-
-    // END OF NEW STUFF
-  });
-
+  res.send(JSON.stringify(queryObject));
 });
-
-
-
-
-
-
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
