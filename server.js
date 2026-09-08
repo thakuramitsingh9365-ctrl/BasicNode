27f1828e3d140a939cb87a7059342cf073014cc4
thakuramitsingh9365-ctrl <thakuramitsingh9365@gmail.com>
2026-09-08 14:08:03 +0530

    Updating now

--- server.js
+++ server.js
@@ -1,21 +1,71 @@
-const express = require("express");
-const server = express();
-const hostname = "127.0.0.1";
+const mysql = require('mysql2');
+const express = require('express');
+const app = express();
 const port = 3000;
-server.get("/", (req, res) => {
-  const queryObject = req.query;
-  console.log(queryObject.a);
-  let props=Object.getOwnPropertyNames(queryObject);
-  console.log(props);
-  props.forEach(element => {
-    console.log(element);
-    console.log(queryObject[element]);
-  });
-  res.setHeader("Content-Type", "application/json");
 
-  res.send(JSON.stringify(queryObject));
+
+
+
+
+
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: '1234',
+  database: 'node'
 });
+connection.connect();
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+
+
+
+
+
+
+
+
+app.get('/select', (req, res) => {
+
+  res.send("select");
+});
+app.get('/delete', (req, res) => {
+
+  res.send("delete");
+});
+app.get('/update', (req, res) => {
+  console.log(req.query);
+  const { bookname, author, genre ,id} = req.query;
+  console.log(req.query);
+  const sql = 'update  books set bookname=?, author=?, genre=? where id=?';
+ result= connection.query(sql,[bookname,author,genre,id]);
+ console.log(result);
+
+  res.send("update");
+});
+//************************************************************* */
+app.get('/insert', (req, res) => {
+
+
+  const { bookname, author, genre } = req.query;
+  console.log(req.query);
+  const sql = 'INSERT INTO books (bookname, author, genre) VALUES (?, ?, ?)';
+  connection.query(sql, [bookname, author, genre], (err, result) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    res.status(200).send({
+      id: result.insertId, booknamecls
+      , author, genre
+    });
+  })
+}
+
+  
+);
+//********************************************************************* */
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
 });
