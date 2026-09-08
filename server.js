2891eccac27e7bce198b8e9b820bc371df67dd46
thakuramitsingh9365-ctrl <thakuramitsingh9365@gmail.com>
2026-09-08 12:11:27 +0530

    Query Object

--- server.js
+++ server.js
@@ -1,63 +1,21 @@
-const mysql = require("mysql2");
-const ejs = require("ejs");
-const fs = require("fs");
-const http = require("http");
+const express = require("express");
+const server = express();
+const hostname = "127.0.0.1";
+const port = 3000;
+server.get("/", (req, res) => {
+  const queryObject = req.query;
+  console.log(queryObject.a);
+  let props=Object.getOwnPropertyNames(queryObject);
+  console.log(props);
+  props.forEach(element => {
+    console.log(element);
+    console.log(queryObject[element]);
+  });
+  res.setHeader("Content-Type", "application/json");
 
-// Create a MySQL connection
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: "3306",
-  user: "root",
-  password: "1234",
-  database: "node",
+  res.send(JSON.stringify(queryObject));
 });
 
-// Connect to the MySQL server
-connection.connect((error) => {
-  if (error) throw error;
-  console.log("Connected to MySQL server");
-
-  // Query the table
-  connection.query("SELECT * FROM data", (error, results) => {
-    if (error) throw error;
-
-    // Read the EJS template file
-    fs.readFile("views/template.ejs", "utf8", (error, template) => {
-      if (error) throw error;
-
-      // Render the EJS template with data from the MySQL table
-      const html = ejs.render(template, { data: results });
-      const output = `output${Math.random()}.html`;
-      // Save the rendered HTML to a file
-      fs.writeFile(output, html, "utf8", (error) => {
-        if (error) throw error;
-        console.log("HTML file generated successfully");
-
-        // Create a simple HTTP server
-        const server = http.createServer((req, res) => {
-          // Read the generated HTML file
-          fs.readFile(output, "utf8", (error, content) => {
-            if (error) {
-              res.writeHead(500);
-              res.end("Error reading HTML file");
-            } else {
-              res.writeHead(200, { "Content-Type": "text/html" });
-              res.end(content);
-            }
-          });
-        });
-
-        // Start the server on port 3000
-        server.listen(3000, () => {
-          console.log("Server running at http://localhost:3000/");
-        });
-
-        // Close the MySQL connection
-        connection.end((error) => {
-          if (error) throw error;
-          console.log("Disconnected from MySQL server");
-        });
-      });
-    });
-  });
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
